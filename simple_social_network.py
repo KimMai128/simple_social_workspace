bce2b3ad43736167b76184bdbe11a4a14aaa34da
Kim Mai <kmai7@terpmail.umd.edu>
2025-03-24 15:27:45 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,14 @@
+class Person:
+    """ A person in the social network.
+    
+    Attributes:
+        name (str): the person's name.
+        connection (set of Person): other people in the social network
+            who know this person.
+    """
+    def __init__(self, name):
+        """ Initialize a new Person objet."""
+        self.name = name
+        self.connection = set()
+
+    
